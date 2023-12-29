a240a88b1bcb8d60b363326f7ff9ada6fa46af60
Farizki Kurniawan <fariski.mail@gmail.com>
2023-12-29 22:21:48 +0700

    feat: add pages

---
+++ lib/page/calculator.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+      body: const Center(
+        child: Text('Calculator'),
+      ),
+    );
+  }
+}
---
+++ lib/page/guide.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Guide extends StatefulWidget {
+  const Guide({super.key});
+
+  @override
+  State<Guide> createState() => _GuideState();
+}
+
+class _GuideState extends State<Guide> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Guides'),
+      ),
+      body: const Center(
+        child: Text('Guides'),
+      ),
+    );
+  }
+}
