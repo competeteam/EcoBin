cf8ed8a17d1bd1a57a318f37b7534d2470f3a408
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-01-08 06:20:26 +0700

    refactor: change name to UserModel, add new fields

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,34 +4,48 @@ import 'model.dart';
 
 enum UserType { user, admin }
 
-class User implements Model {
+class UserModel implements Model {
   final Timestamp? deletedAt;
   final Timestamp? createdAt;
-  final String? name;
+  final String? uid;
+  final String? displayName;
   final String? email;
   final String? password;
+  final String? photoURL;
+
+  final bool? isEmailVerified;
+  final bool? isAnonymous;
   final UserType? type;
 
-  User(
-      {this.deletedAt,
-      this.createdAt,
-      this.name,
-      this.email,
-      this.password,
-      this.type});
+  UserModel({
+    this.deletedAt,
+    this.createdAt,
+    this.uid,
+    this.displayName,
+    this.email,
+    this.password,
+    this.photoURL,
+    this.isEmailVerified,
+    this.isAnonymous,
+    this.type,
+  });
 
-  factory User.fromFirestore(
+  factory UserModel.fromFirestore(
     DocumentSnapshot<Map<String, dynamic>> snapshot,
     SnapshotOptions? options,
   ) {
     final data = snapshot.data();
 
-    return User(
+    return UserModel(
         deletedAt: data?['deletedAt'],
         createdAt: data?['createdAt'],
-        name: data?['name'],
+        uid: data?['uid'],
+        displayName: data?['displayName'],
         email: data?['email'],
         password: data?['password'],
+        photoURL: data?['photoURL'],
+        isEmailVerified: data?['isEmailVerified'],
+        isAnonymous: data?['isAnonymous'],
         type: data?['type']);
   }
 
@@ -40,9 +54,13 @@ class User implements Model {
     return {
       if (deletedAt != null) "deletedAt": deletedAt!.seconds,
       if (createdAt != null) "createdAt": createdAt!.seconds,
-      if (name != null) "name": name,
+      if (uid != null) "uid": uid,
+      if (displayName != null) "displayName": displayName,
       if (email != null) "email": email,
       if (password != null) "password": password,
+      if (photoURL != null) "photoURL": photoURL,
+      if (isEmailVerified != null) "isEmailVerified": isEmailVerified,
+      if (isAnonymous != null) "isAnonymous": isAnonymous,
       if (type != null) "type": type.toString().split('.').last,
     };
   }
