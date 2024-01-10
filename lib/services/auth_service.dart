9806df1e6a28be3b8bd9374313c33ae2a9b85c14
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-01-11 05:15:21 +0700

    feat: login and register with email functionality

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:dinacom_2024/services/user_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/user.dart';
@@ -10,7 +11,12 @@ class AuthService {
     return user == null
         ? null
         : UserModel(
+            createdAt: user.metadata.creationTime,
             uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            isEmailVerified: user.emailVerified,
+            isAnonymous: user.isAnonymous,
           );
   }
 
@@ -22,7 +28,7 @@ class AuthService {
   }
 
   // sign in anonymously
-  Future signInAnon() async {
+  Future logInAnon() async {
     try {
       UserCredential credential = await _authService.signInAnonymously();
 
@@ -31,16 +37,55 @@ class AuthService {
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
+
       return null;
     }
   }
 
-// sign in with email & password
+  // sign in with email & password
+  Future logInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _authService.signInWithEmailAndPassword(
+          email: email, password: password);
 
-// register with email & password
+      User? user = credential.user;
 
-// sign out
-  Future signOut() async {
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+
+      return null;
+    }
+  }
+
+  // register with email & password
+  Future registerWithEmailAndPassword(
+      String name, String email, String password) async {
+    try {
+      UserCredential credential = await _authService
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      User? user = credential.user;
+
+      await UserService().addUser(
+        createdAt: user!.metadata.creationTime,
+        uid: user!.uid,
+        displayName: user.displayName ?? '',
+        email: user.email ?? '',
+        isEmailVerified: user.emailVerified,
+        isAnonymous: user.isAnonymous,
+      );
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+
+      return null;
+    }
+  }
+
+  // sign out
+  Future logOut() async {
     try {
       return await _authService.signOut();
     } catch (e) {
