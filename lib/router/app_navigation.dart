4bb044d2e7efdf825e73a3e61c79016b896e540f
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-01-11 05:14:55 +0700

    refactor: move /login and /register to root

--- lib/router/app_navigation.dart
+++ lib/router/app_navigation.dart
@@ -5,8 +5,9 @@ import 'package:dinacom_2024/page/calculator.dart';
 import 'package:dinacom_2024/page/classificator.dart';
 import 'package:dinacom_2024/page/garbages.dart';
 import 'package:dinacom_2024/page/guide.dart';
-import 'package:dinacom_2024/page/login.dart';
-import 'package:dinacom_2024/page/profile.dart';
+import 'package:dinacom_2024/page/profile/login.dart';
+import 'package:dinacom_2024/page/profile/profile.dart';
+import 'package:dinacom_2024/page/profile/register.dart';
 import 'package:dinacom_2024/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -24,87 +25,105 @@ class AppNavigation {
       GlobalKey<NavigatorState>(debugLabel: 'guidesNavigator');
   static final _profileNavigatorKey =
       GlobalKey<NavigatorState>(debugLabel: 'profileNavigator');
+  static final _loginNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'loginNavigator');
+  static final _registerNavigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'registerNavigator');
 
   static final _router = GoRouter(
-      debugLogDiagnostics: true,
-      navigatorKey: _rootNavigatorKey,
-      initialLocation: '/garbage',
-      routes: <RouteBase>[
-        StatefulShellRoute.indexedStack(
-            builder: (context, state, navigationShell) {
-              return Wrapper(navigationShell: navigationShell);
-            },
-            branches: <StatefulShellBranch>[
-              // Calculator
-              StatefulShellBranch(
-                  navigatorKey: _calculatorNavigatorKey,
-                  routes: <RouteBase>[
-                    GoRoute(
-                      name: 'Calculator',
-                      path: '/calculator',
-                      builder: (context, state) => const Calculator(),
-                    )
-                  ]),
+    debugLogDiagnostics: true,
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/garbage',
+    routes: <RouteBase>[
+      StatefulShellRoute.indexedStack(
+          builder: (context, state, navigationShell) {
+            return Wrapper(navigationShell: navigationShell);
+          },
+          branches: <StatefulShellBranch>[
+            // Calculator
+            StatefulShellBranch(
+                navigatorKey: _calculatorNavigatorKey,
+                routes: <RouteBase>[
+                  GoRoute(
+                    name: 'Calculator',
+                    path: '/calculator',
+                    builder: (context, state) => const Calculator(),
+                  )
+                ]),
 
-              // Classificator
-              StatefulShellBranch(
-                  navigatorKey: _classificatorNavigatorKey,
-                  routes: <RouteBase>[
-                    GoRoute(
-                        name: 'Classificator',
-                        path: '/classificator',
-                        builder: (context, state) => const Classificator(),
-                        routes: <RouteBase>[
-                          GoRoute(
-                            name: 'Manual Classificator',
-                            path: 'manual',
-                            builder: (context, state) =>
-                                const ManualClassificator(),
-                          ),
-                          GoRoute(
-                            name: 'Automatic Classificator',
-                            path: 'automatic',
-                            builder: (context, state) =>
-                                const AutomaticClassificator(),
-                          )
-                        ])
-                  ]),
+            // Classificator
+            StatefulShellBranch(
+                navigatorKey: _classificatorNavigatorKey,
+                routes: <RouteBase>[
+                  GoRoute(
+                      name: 'Classificator',
+                      path: '/classificator',
+                      builder: (context, state) => const Classificator(),
+                      routes: <RouteBase>[
+                        GoRoute(
+                          name: 'Manual Classificator',
+                          path: 'manual',
+                          builder: (context, state) =>
+                              const ManualClassificator(),
+                        ),
+                        GoRoute(
+                          name: 'Automatic Classificator',
+                          path: 'automatic',
+                          builder: (context, state) =>
+                              const AutomaticClassificator(),
+                        )
+                      ])
+                ]),
 
-              // Garbage
-              StatefulShellBranch(
-                  navigatorKey: _garbageNavigatorKey,
-                  routes: <RouteBase>[
-                    GoRoute(
-                      name: 'Garbage',
-                      path: '/garbage',
-                      builder: (context, state) => const Garbages(),
-                    )
-                  ]),
+            // Garbage
+            StatefulShellBranch(
+                navigatorKey: _garbageNavigatorKey,
+                routes: <RouteBase>[
+                  GoRoute(
+                    name: 'Garbage',
+                    path: '/garbage',
+                    builder: (context, state) => const Garbages(),
+                  )
+                ]),
 
-              // Guides
-              StatefulShellBranch(
-                  navigatorKey: _guidesNavigatorKey,
-                  routes: <RouteBase>[
-                    GoRoute(
-                      name: 'Guides',
-                      path: '/guides',
-                      builder: (context, state) => const Guide(),
-                    )
-                  ]),
+            // Guides
+            StatefulShellBranch(
+                navigatorKey: _guidesNavigatorKey,
+                routes: <RouteBase>[
+                  GoRoute(
+                    name: 'Guides',
+                    path: '/guides',
+                    builder: (context, state) => const Guide(),
+                  )
+                ]),
 
-              // Profile
-              StatefulShellBranch(
-                  navigatorKey: _profileNavigatorKey,
+            // Profile
+            StatefulShellBranch(
+                navigatorKey: _profileNavigatorKey,
                 routes: <RouteBase>[
                   GoRoute(
                     name: 'Profile',
                     path: '/profile',
                     builder: (context, state) => const Profile(),
                   )
-                ]
-              ),
-            ]),
-      ]);
+                ]),
+          ]),
+
+      // Login
+      GoRoute(
+        name: 'Login',
+        path: '/login',
+        builder: (context, state) => const Login(),
+      ),
+
+      // Register
+      GoRoute(
+        name: 'Register',
+        path: '/register',
+        builder: (context, state) => const Register(),
+      )
+    ],
+  );
 
   static GoRouter getRouter() {
     return _router;
