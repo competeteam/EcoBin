30a2612ec71ee866bd55bfb050b3af6e2cc0d84e
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-01-14 08:27:23 +0700

    feat: initial commit

---
+++ lib/components/error_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: const Color(0xFF222222),
+        child: const Center(
+            child: Text('Error'),
+        )
+    );
+  }
+}
