b10172a76ae7f9bd723a8fa4e88a40de19a9ae91
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-01-15 00:37:07 +0700

    feat: add tree question structure

--- lib/features/classificator/manual.dart
+++ lib/features/classificator/manual.dart
@@ -1,6 +1,14 @@
 import 'package:dinacom_2024/components/classificator/result_card.dart';
 import 'package:flutter/material.dart';
 
+class Question {
+  final String text;
+  final Question? yes;
+  final Question? no;
+
+  Question(this.text, {this.yes, this.no});
+}
+
 class ManualClassificator extends StatefulWidget {
   const ManualClassificator({super.key});
 
@@ -9,6 +17,50 @@ class ManualClassificator extends StatefulWidget {
 }
 
 class _ManualClassificatorState extends State<ManualClassificator> {
+  late List<String> questions;
+  late Question questionTree;
+
+  @override
+  void initState() {
+    super.initState();
+
+    questions = [
+      'Is your waste made of organic matter or derived from living organism?',
+      'Is your waste used for writing printing, or packaging?',
+      'Is your waste liquid?',
+      'Is your waste made of soft or mallable materials?',
+      'Is your waste transparent or slightly transparent?',
+      'Is your waste made of electronic components?',
+    ];
+
+    questionTree =
+        Question(
+          questions[0], 
+          yes: Question(
+            questions[1],
+            yes: Question('Paper'),
+            no: Question('Organic'),
+          ), 
+          no: Question(
+            questions[2],
+            yes: Question('Chemical'),
+            no: Question(
+              questions[3],
+              yes: Question('Plastic'),
+              no: Question(
+                questions[4],
+                yes: Question('Glass'),
+                no: Question(
+                  questions[5],
+                  yes: Question('EWaste'),
+                  no: Question('Metal'),
+                )
+              )
+            )
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const ClassificatorResultCard('');
