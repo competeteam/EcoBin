f65962078084a2bab3cd83367aefd121bfd679a1
Michael Utama <michaelu670@gmail.com>
2024-01-18 19:15:44 +0700

    feat: local guide content

--- lib/page/guide.dart
+++ lib/page/guide.dart
@@ -12,6 +12,34 @@ class Guide extends StatefulWidget {
 }
 
 class _GuideState extends State<Guide> {
+  static final List<GuideContent> localContents = <GuideContent>[
+    GuideContent(
+      title: "Simple Ways of Waste Management at Home",
+      content: "To reduce the production of such waste, it’s important to practice waste management at home. You can make small changes in your home which can lead to a big impact. Take a look at these simple ways in which waste can be efficiently managed at home:\n"
+      "\n"
+      "Limit the use of plastic: \nAvoid buying plastic water bottles; instead, carry your own water bottle whenever you step out. Do away with plastic straws and drink directly from the glass. Use steel or glass cups instead of plastic ones and carry a cloth bag with you every time you go out shopping. You can even turn your old jeans or pieces of denim into bags.\n"
+      "\n"
+      "Segregate the waste: \nSegregate garbage into degradable and non-degradable waste. You can also create compost at home with leftover food, fruit and vegetable peels etc. Waste segregation not only makes the process of recycling much easier, but it will also help in maintaining a healthy and clean surrounding.\n"
+      "\n"
+      "Reduce the use of paper: \nUse cloth rags instead of paper towels in the kitchen. Maintain soft copies of your journals, rather than using a notebook or a diary. Buy e-books instead of hard copies. Switch to using handkerchiefs and avoiding tissue papers to wipe your hands and face. Prefer metal or eco-friendly bamboo jute plates and cutlery instead of using paper plates when needed.\n"
+      "\n"
+      "Say yes to composting: \nComposting can reduce wastes by turning wet waste into fertilisers for plants. Besides, if you have or are planning to grow your own garden, you will have homemade, eco-friendly, chemical-free fertilisers to nourish your green babies.\n"
+      "\n"
+      "Plan your meals in advance: \nA lot of waste is generated from people throwing excess food away because they do not plan their meals in advance.\n"
+      "\n"
+      "Source: https://www.hdfc.com/blog/sustainability-at-hdfc/7-simple-ways-waste-management-home",
+      imagePath: "",
+    ),
+    GuideContent(
+      title: "Classifying Waste",
+      content: "There are 6 types of waste that can be classified into two categories: recyclable and non-recyclable.\n\n"
+          "The recyclable waste includes paper, cardboard, glass, metal, and plastic.\n\n"
+          "The non-recyclable waste includes hazardous waste, medical waste, electronic waste, organic waste, and general waste .\n\n"
+          "It is important to follow the rules when disposing of waste. For example, green waste must not contain logs, stumps, palm logs, whole trees, mulched trees, golden cane trunks, soil, or turf. Branches must be under 13cm diameter. All green waste must stay under the top rail of the skip bin, with nothing poking up. General waste must not contain rocks, bricks, tiles, concrete, pavers, hard wood, paint, chemicals, tyres, carpet, mattress, soil, dirt, sand or hard fill. All general waste must stay under the top rail of the skip bin, with nothing poking up.\n\n"
+          ""
+          "Source: https://4waste.com.au/rubbish-removal/5-types-waste-know/"
+    )
+  ];
   static List<GuideContent> contents = <GuideContent>[
 
   ];
@@ -21,7 +49,11 @@ class _GuideState extends State<Guide> {
   List<GuideCard> cards = [];
 
   Future<void> _replaceALlGuide() async {
-    contents = await GuideService().getAllGuides() ?? [];
+    var onlineContent = await GuideService().getAllGuides() ?? [];
+    contents.clear();
+    contents.addAll(localContents);
+    contents.addAll(onlineContent);
+
     setState(() {
       int p = 0;
       cards = contents.map((e) => GuideCard(
@@ -112,11 +144,11 @@ class GuideCard extends StatelessWidget {
                 ),
               ),
               subtitle: Text(subtitle,
-                maxLines: 4,
+                maxLines: 3,
                 overflow: TextOverflow.ellipsis,
                 style: const TextStyle(
                   color: Colors.white,
-                  fontSize: 10,
+                  fontSize: 14,
                   fontFamily: 'Inter',
                   fontWeight: FontWeight.w300,
                   height: 0,
@@ -165,10 +197,9 @@ class GuideArticle extends StatelessWidget {
           children: <Widget>[
             Text(guideContent.title ?? 'Guide not found',
               overflow: TextOverflow.fade,
-              maxLines: 3,
               style: const TextStyle(
                 color: Colors.white,
-                fontSize: 40,
+                fontSize: 32,
                 fontFamily: 'Inter',
                 fontWeight: FontWeight.w500,
                 height: 0,
